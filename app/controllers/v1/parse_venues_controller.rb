b16943ffea69f4110f33c2e42dbacf23fad046e7
Benjamin Sinaiko <ben.jamin@outlook.com>
2018-02-05 14:10:42 -0600

    Add route and method for get_events

--- app/controllers/v1/parse_venues_controller.rb
+++ app/controllers/v1/parse_venues_controller.rb
@@ -1,2 +1,11 @@
 class V1::ParseVenuesController < ApplicationController
+
+  def get_venues
+    response = Unirest.get("https://parseapi.back4app.com/classes/Venues?limit=200",
+      headers:{ "X-Parse-Application-Id" => "#{ENV['PROD_APP_ID']}",
+      "X-Parse-REST-API-Key" => "#{ENV['PROD_API_KEY']}"})
+    venues = response.body["results"]
+    render json: venues.as_json
+  end
+
 end
